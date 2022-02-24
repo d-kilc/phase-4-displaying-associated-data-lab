19335b5c86bcf0dbce49ad33aa1c96184acf6205
Daniel <dkilcullen25@gmail.com>
2022-02-24 12:48:39 -0600

    commit tree 839f827add613932bd4cbffc28082a7c6184f271

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,14 @@
 class UsersController < ApplicationController
+    def show
+        user = find_user
+        render json: user, include: :items
+    rescue ActiveRecord::RecordNotFound => invalid
+        render json: invalid.record.errors
+    end
+
+    private
+
+    def find_user
+        user = User.find params[:id]
+    end
 end
